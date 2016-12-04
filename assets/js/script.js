617d0ef3eb07269b30453c266aa941b736d8ba22
Danny van Kooten <dannyvankooten@gmail.com>
2016-12-04 18:37:40 +0100

    focus input on toggle & clear message timeout when setting new one

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,19 @@
     'use strict';
 
     var opts = change_username;
+    var timeout;
     var mount = document.querySelector('.user-user-login-wrap td .description');
-    var usernameInput = document.getElementById('user_login');
-
+    var currentUsernameInput = document.getElementById('user_login');
     var link = el('a', { href: '#', onclick: toggle }, 'Change' );
+    var newUsernameInput = el('input', { type: "text", name: "new_user_login", value: currentUsernameInput.value, className: "regular-text" , style: { "min-height": "28px" }, autocomplete: "off" });
+    var submitButton = el('input', { type: "button", value: "Change", className: "button", onclick: onSubmit });
     var form = el('form', {
         method: "POST",
         onsubmit: onSubmit,
         style: { display: "none" }
     }, [
-        el('input', { type: "text", name: "new_user_login", value: usernameInput.value, className: "regular-text" , style: { "min-height": "28px" }}),
-        el('input', { type: "button", value: "Change", className: "button", onclick: onSubmit })
+        newUsernameInput,
+        submitButton
     ]);
     var message = el('p', { style: {
         display: 'none'
@@ -70,13 +72,14 @@
         e.preventDefault();
 
         var new_username = form.new_user_login.value;
-        var current_username = usernameInput.value;
+        var current_username = currentUsernameInput.value;
 
         // do nothing if username is very short or unchanged
         if( new_username.length < 2 || new_username === current_username ) {
             return;
         }
 
+        message.style.display = 'none';
         var data = 'current_username='+encodeURIComponent(current_username)+'&new_username='+encodeURIComponent(new_username)+'&_ajax_nonce='+encodeURIComponent(opts.nonce);
         var request = new XMLHttpRequest();
         request.open('POST', opts.ajaxurl + "?action=change_username", true);
@@ -98,7 +101,7 @@
                     message.innerHTML = data.message;
 
                     if( data.success ) {
-                        usernameInput.value = new_username;
+                        currentUsernameInput.value = new_username;
                         toggle();
                     }
                 } catch(e) {}
@@ -109,8 +112,12 @@
                 message.textContent = "Uh oh, something went wrong submitting the form.";
             }
 
+            // clear message timeout & set a new one
+            if(timeout) {
+                window.clearTimeout(timeout);
+            }
             message.style.display = '';
-            window.setTimeout(function() { message.style.display = 'none'; }, 6000);
+            timeout = window.setTimeout(function() { message.style.display = 'none'; }, 6000);
         };
         request.send(data);
     }
@@ -128,12 +135,13 @@
         if( form.style.display === 'none' ) {
             form.style.display = '';
             link.style.display = 'none';
-            usernameInput.style.display = 'none';
+            currentUsernameInput.style.display = 'none';
+            newUsernameInput.focus();
             document.addEventListener('keydown', toggleOnEscape);
         } else {
             form.style.display = 'none';
             link.style.display = 'inline';
-            usernameInput.style.display = '';
+            currentUsernameInput.style.display = '';
             document.removeEventListener('keydown', toggleOnEscape);
         }
     }
