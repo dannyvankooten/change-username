84cbaee6b2cb05dc173348208706703a5e0729a2
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-23 14:43:15 +0100

    minor JS refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
 (function() {
-    'use strict';
-
-    var opts = change_username;
-    var timeout;
-    var mount = document.querySelector('.user-user-login-wrap td .description');
-    var currentUsernameInput = document.getElementById('user_login');
-    var link = el('a', { href: '#', onclick: toggle }, 'Change' );
-    var newUsernameInput = el('input', { type: "text", name: "new_user_login", value: currentUsernameInput.value, className: "regular-text" , style: { "min-height": "28px" }, autocomplete: "off" });
-    var submitButton = el('input', { type: "button", value: "Change", className: "button", onclick: onSubmit });
-    var form = el('form', {
+    const opts = window.change_username;
+    let timeout;
+    const mount = document.querySelector('.user-user-login-wrap td .description');
+    const currentUsernameInput = document.getElementById('user_login');
+    const link = el('a', { href: '#', onclick: toggle }, 'Change' );
+    const newUsernameInput = el('input', { type: "text", name: "new_user_login", value: currentUsernameInput.value, className: "regular-text" , style: { "min-height": "28px" }, autocomplete: "off" });
+    const submitButton = el('input', { type: "button", value: "Change", className: "button", onclick: onSubmit });
+    const form = el('form', {
         method: "POST",
         onsubmit: onSubmit,
         style: { display: "none" }
@@ -16,7 +14,7 @@
         newUsernameInput,
         submitButton
     ]);
-    var message = el('p', { style: {
+    const message = el('p', { style: {
         display: 'none'
     }});
 
@@ -24,11 +22,11 @@
     link.parentNode.appendChild(el('div', [ form, message ]));
 
     /**
-     * @param object
-     * @param attrs
+     * @param {object} object
+     * @param {object} attrs
      */
     function setAttributes(object, attrs) {
-        for(var key in attrs) {
+        for(const key in attrs) {
             if(typeof(attrs[key]) === "object") {
                 setAttributes(object[key], attrs[key]);
             } else {
@@ -38,13 +36,13 @@
     }
     /**
      *
-     * @param name
-     * @param attrs
-     * @param children
-     * @returns {Element}
+     * @param {string} name
+     * @param {object|HTMLElement[]} attrs
+     * @param {HTMLElement[]} [children]
+     * @returns {HTMLElement}
      */
     function el(name, attrs, children) {
-        var e = document.createElement(name);
+        const e = document.createElement(name);
 
         if( !children && ( Array.isArray(attrs) || typeof(attrs) === "string")) {
             children = attrs;
@@ -59,7 +57,7 @@
             if(typeof(children) === "string") {
                 e.textContent = children;
             } else {
-               for(var i=0; i<children.length; i++) {
+               for(let i=0; i < children.length; i++) {
                 e.appendChild(children[i]);
                }
             }
@@ -68,11 +66,11 @@
         return e;
     }
 
-    function onSubmit(e) {
-        e.preventDefault();
+    function onSubmit(evt) {
+        evt.preventDefault();
 
-        var new_username = form.new_user_login.value;
-        var current_username = currentUsernameInput.value;
+        const new_username = form.new_user_login.value;
+        const current_username = currentUsernameInput.value;
 
         // do nothing if username is very short or unchanged
         if( new_username.length < 2 || new_username === current_username ) {
@@ -81,17 +79,17 @@
 
         submitButton.value = "Please wait..";
         submitButton.disabled = true;
-        var data = 'current_username='+encodeURIComponent(current_username)+'&new_username='+encodeURIComponent(new_username)+'&_ajax_nonce='+encodeURIComponent(opts.nonce);
-        var request = new XMLHttpRequest();
+        const data = 'current_username='+encodeURIComponent(current_username)+'&new_username='+encodeURIComponent(new_username)+'&_ajax_nonce='+encodeURIComponent(opts.nonce);
+        const request = new XMLHttpRequest();
         request.open('POST', opts.ajaxurl + "?action=change_username", true);
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         request.onload = function() {
             submitButton.value = "Change";
             submitButton.disabled = false;
-            var errored = true;
+            let errored = true;
             if (request.status >= 200 && request.status < 400 && request.responseText != -1 ) {
                 try {
-                    var data = JSON.parse(request.responseText);
+                    const data = JSON.parse(request.responseText);
 
                     // we're good.
                     errored = false;
@@ -125,14 +123,23 @@
         request.send(data);
     }
 
-    function toggleOnEscape(e) {
-         if(e.keyCode == 27 ) { toggle(); }
+    /**
+     * Toggles when ESC is pressed
+     *
+     * @param {KeyboardEvent} evt
+     */
+    function toggleOnEscape(evt) {
+         if(evt.keyCode === 27 ) { toggle(); }
     }
 
-    // toggle between link / form
-    function toggle(e) {
-        if(e) {
-            e.preventDefault();
+    /**
+     * Toggle between link element and form element
+     *
+     * @param {MouseEvent=} evt
+     */
+    function toggle(evt) {
+        if(evt) {
+            evt.preventDefault();
         }
 
         if( form.style.display === 'none' ) {
